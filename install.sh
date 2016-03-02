2d8eb329e29022fe5d7eceb923a631e8758208a1
Shokman <jchoclin@creativa77.com.ar>
2016-03-02 19:12:39 -0300

    Correct scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
+#/bin/bash
 
+# Download dependencies
+sudo apt-get install exuberant-ctags ruby ruby-dev -y
+sudo pip install flake8
 
-sudo apt-get install exuberant-ctags ruby ruby-dev -y 
-
+# Initialize submodules
 git submodule init && git submodule update
 
-cd ~/.vim/bundle/YouCompleteMe
+# Finish YouCompleteMe installation
+cd ~/.vim/bundle/youcompleteme
+git submodule update --init --recursive
 ./install.py --clang-completer
+cd ~/.vim
 
+# Configure vimrc
 cp myvimrc ../.vimrc
